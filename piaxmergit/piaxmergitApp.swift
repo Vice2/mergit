0b8a7bd324c162cbbb30976a2876eb27a182974b
Vice2 <90139249+Vice2@users.noreply.github.com>
2021-11-04 13:06:19 +0100

    Initial Commit

---
+++ piaxmergit/piaxmergitApp.swift
@@ -0,0 +1,17 @@
+//
+//  piaxmergitApp.swift
+//  piaxmergit
+//
+//  Created by Bashar Al Bagdadi on 2021-11-04.
+//
+
+import SwiftUI
+
+@main
+struct piaxmergitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
